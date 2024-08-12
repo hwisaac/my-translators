9a833dff80dcd377e45d635435cb3fa87f0f7724
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-08-12 11:29:44 +0900

    0812

--- src/app/api/naver/route.ts
+++ src/app/api/naver/route.ts
@@ -5,9 +5,17 @@ export async function POST(request: Request) {
   const { sourceText } = await request.json();
   if (!sourceText) throw new Error('sourceText 가 비었습니다.');
   console.log(`요청받음🤔 [papago] ${sourceText}`);
+  console.log(
+    'process.env.DB_PAPAGO_CLIENT_ID',
+    process.env.DB_PAPAGO_CLIENT_ID
+  );
+  console.log(
+    'process.env.DB_PAPAGO_CLIENT_SECRET',
+    process.env.DB_PAPAGO_CLIENT_SECRET
+  );
   const response = await axios
     .post(
-      'https://openapi.naver.com/v1/papago/n2mt',
+      'https://naveropenapi.apigw.ntruss.com/nmt/v1/translation',
       {
         source: 'en',
         target: 'ko',
@@ -16,13 +24,17 @@ export async function POST(request: Request) {
       {
         headers: {
           'Content-Type': 'application/json',
-          'X-Naver-Client-Id': process.env.DB_PAPAGO_CLIENT_ID,
-          'X-Naver-Client-Secret': process.env.DB_PAPAGO_CLIENT_SECRET,
+          'X-NCP-APIGW-API-KEY-ID	': process.env.DB_PAPAGO_CLIENT_ID,
+          'X-NCP-APIGW-API-KEY	': process.env.DB_PAPAGO_CLIENT_SECRET,
         },
       }
     )
-    .then((res) => res.data);
-
+    .then((res) => res.data)
+    .catch((error) => {
+      console.log(error.response.data);
+      console.log(error.response.status);
+    });
+  
   return new Response(
     JSON.stringify({
       text: response.message.result.translatedText,
